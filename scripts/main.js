8259599621bc9e32a66bcdfc6b3fb7467fa97c10
Ray Orunsolu <rorunsolu@gmail.com>
2024-12-10 21:08:32 +0000

    mobile menu updated, no longer using inline scripts

--- scripts/main.js
+++ scripts/main.js
@@ -9,4 +9,41 @@ import '/styles/offer.scss';
 import '/styles/skills.scss';
 import '/styles/contact.scss';
 
+import '/styles/button.scss';
+
 import '/styles/footer.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const openNavBtn = document.querySelector('.nav__toggle-open');
+    const popupContainer = document.querySelector('.popup-container');
+
+    openNavBtn.addEventListener('click', () => {
+        if (popupContainer) {
+            popupContainer.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+        }
+    });
+
+    const closeNavBtn = popupContainer.querySelector('.nav__toggle-close');
+
+    closeNavBtn.addEventListener('click', () => {
+        popupContainer.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    });
+
+    const navLinks = popupContainer.querySelectorAll('.nav__link');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            popupContainer.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        });
+    });
+
+    popupContainer.addEventListener('click', (event) => {
+        if (event.target === popupContainer) {
+            popupContainer.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        }
+    });
+});
