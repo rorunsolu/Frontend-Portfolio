706ca52969e99d75f40c3a8c0224dc0250cfc6a3
Ray Orunsolu <rorunsolu@gmail.com>
2024-12-25 23:53:57 +0000

    chnaged file paths

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import '../styles/style.scss';
+import './styles/style.scss';
 
-import '../styles/header.scss';
-import '../styles/navbar.scss';
+import './styles/header.scss';
+import './styles/navbar.scss';
 
-import '../styles/experience.scss';
-import '../styles/projects.scss';
-import '../styles/about.scss';
-import '../styles/offer.scss';
-import '../styles/skills.scss';
-import '../styles/contact.scss';
+import './styles/experience.scss';
+import './styles/projects.scss';
+import './styles/about.scss';
+import './styles/offer.scss';
+import './styles/skills.scss';
+import './styles/contact.scss';
 
-import '../styles/button.scss';
+import './styles/button.scss';
 
-import '../styles/footer.scss';
+import './styles/footer.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
     const openNavBtn = document.querySelector('.nav__toggle-open');
