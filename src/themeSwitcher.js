9a81d85bddf909ecd7f262c68ab10f6e59b56c0f
Ray Orunsolu <rorunsolu@gmail.com>
2025-01-28 21:15:31 +0000

    dark theme is now the default

---
+++ src/themeSwitcher.js
@@ -0,0 +1,19 @@
+let lightmode = localStorage.getItem('lightmode');
+const themeSwitch = document.getElementById('theme-switch');
+
+const enablelightmode = () => {
+    document.body.classList.add('lightmode');
+    localStorage.setItem('lightmode', 'active');
+};
+
+const disablelightmode = () => {
+    document.body.classList.remove('lightmode');
+    localStorage.setItem('lightmode', null);
+};
+
+if (lightmode === "active") enablelightmode();
+
+themeSwitch.addEventListener("click", () => {
+    lightmode = localStorage.getItem('lightmode');
+    lightmode !== "active" ? enablelightmode() : disablelightmode();
+});
