b8e3008fdd96b3268fef06d731455c187a84b4fa
Ray Orunsolu <rorunsolu@gmail.com>
2025-07-21 00:34:39 +0100

    many UI changes

--- src/main.js
+++ src/main.js
@@ -1,59 +1,58 @@
-import './styles/style.scss';
+import "./styles/style.scss";
 
-import './styles/header.scss';
-import './styles/navbar.scss';
-import './styles/footer.scss';
-import './styles/sidebar.scss';
+import "./styles/header.scss";
+import "./styles/navbar.scss";
+import "./styles/sidebar.scss";
 
-import './styles/employment.scss';
-import './styles/projects.scss';
-import './styles/about.scss';
-import './styles/offer.scss';
-import './styles/skills.scss';
-import './styles/contact.scss';
+import "./styles/employment.scss";
+import "./styles/projects.scss";
+import "./styles/about.scss";
+import "./styles/offer.scss";
+import "./styles/skills.scss";
+import "./styles/contact.scss";
 
-import './styles/button.scss';
-import './styles/utilities.scss';
-import './styles/typography.scss';
+import "./styles/button.scss";
+import "./styles/utilities.scss";
+import "./styles/typography.scss";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const openNavBtn = document.querySelector('.nav__toggle-open');
-    const popupContainer = document.querySelector('.popup-container');
+document.addEventListener("DOMContentLoaded", () => {
+  const openNavBtn = document.querySelector(".nav__toggle-open");
+  const popupContainer = document.querySelector(".popup-container");
 
-    openNavBtn.addEventListener('click', () => {
-        if (popupContainer) {
-            popupContainer.classList.add('active');
-            document.body.style.overflow = 'hidden';
-        }
+  openNavBtn.addEventListener("click", () => {
+    if (popupContainer) {
+      popupContainer.classList.add("active");
+      document.body.style.overflow = "hidden";
+    }
+  });
+
+  const closeNavBtn = popupContainer.querySelector(".nav__toggle-close");
+
+  closeNavBtn.addEventListener("click", () => {
+    popupContainer.classList.remove("active");
+    document.body.style.overflow = "auto";
+  });
+
+  const navLinks = popupContainer.querySelectorAll(".nav__link");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      popupContainer.classList.remove("active");
+      document.body.style.overflow = "auto";
     });
+  });
 
-    const closeNavBtn = popupContainer.querySelector('.nav__toggle-close');
+  popupContainer.addEventListener("click", (event) => {
+    if (event.target === popupContainer) {
+      popupContainer.classList.remove("active");
+      document.body.style.overflow = "auto";
+    }
+  });
 
-    closeNavBtn.addEventListener('click', () => {
-        popupContainer.classList.remove('active');
-        document.body.style.overflow = 'auto';
+  document.querySelectorAll(".email-link").forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      window.open("mailto:rorunsolu@gmail.com", "_blank");
     });
-
-    const navLinks = popupContainer.querySelectorAll('.nav__link');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            popupContainer.classList.remove('active');
-            document.body.style.overflow = 'auto';
-        });
-    });
-
-    popupContainer.addEventListener('click', (event) => {
-        if (event.target === popupContainer) {
-            popupContainer.classList.remove('active');
-            document.body.style.overflow = 'auto';
-        }
-    });
-
-    document.querySelectorAll('.email-link').forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            window.open('mailto:rorunsolu@gmail.com', '_blank');
-        });
-    });
+  });
 });
