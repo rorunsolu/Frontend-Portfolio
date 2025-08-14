65dd5b88b81a3963b8e333669bbd35f410f65d66
Ray Orunsolu <rorunsolu@gmail.com>
2025-08-14 15:55:56 +0100

    should make lightmode the default because apparantly the whole dark theme "isn't it" and I kinda agree

--- src/themeSwitcher.js
+++ src/themeSwitcher.js
@@ -1,19 +1,23 @@
-let lightmode = localStorage.getItem('lightmode');
-const themeSwitch = document.getElementById('theme-switch');
+let lightmode = localStorage.getItem("lightmode");
+const themeSwitch = document.getElementById("theme-switch");
 
 const enablelightmode = () => {
-    document.body.classList.add('lightmode');
-    localStorage.setItem('lightmode', 'active');
+  document.body.classList.add("lightmode");
+  localStorage.setItem("lightmode", "active");
 };
 
 const disablelightmode = () => {
-    document.body.classList.remove('lightmode');
-    localStorage.setItem('lightmode', null);
+  document.body.classList.remove("lightmode");
+  localStorage.setItem("lightmode", null);
 };
 
-if (lightmode === "active") enablelightmode();
+// if (lightmode === "active") enablelightmode();
+
+if (lightmode !== "null" && lightmode !== null) {
+  enablelightmode();
+}
 
 themeSwitch.addEventListener("click", () => {
-    lightmode = localStorage.getItem('lightmode');
-    lightmode !== "active" ? enablelightmode() : disablelightmode();
+  lightmode = localStorage.getItem("lightmode");
+  lightmode !== "active" ? enablelightmode() : disablelightmode();
 });
